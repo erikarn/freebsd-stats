f14988411a8053c73ad825b6cb8c01fd3c62735d
Adrian Chadd <adrian@freebsd.org>
2013-09-23 16:14:55 -0700

    Flesh out the plugin registration process:
    * add a function to lookup a plugin by name
    * flesh out the register processing

--- src/plugin.c
+++ src/plugin.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <err.h>
 
@@ -8,8 +9,22 @@
 #include "stat_plugin.h"
 #include "stat_instance.h"
 
-TAILQ_HEAD(, stat_plugins) plugins_list;
+TAILQ_HEAD(, stat_plugin) plugins_list;
 TAILQ_HEAD(, stat_instance) instances_list;
+
+static struct stat_plugin *
+stat_plugin_lookup_by_name(const char *name)
+{
+	struct stat_plugin *p;
+
+	TAILQ_FOREACH(p, &plugins_list, node) {
+		if (strlen(name) == strlen(p->name)
+		    && strcmp(name, p->name) == 0) {
+			return p;
+		}
+	}
+	return (NULL);
+}
 
 int
 stat_plugin_register(char *name, stat_plugin_create_func *create_func,
@@ -18,9 +33,26 @@ stat_plugin_register(char *name, stat_plugin_create_func *create_func,
 	struct stat_plugin *p;
 
 	/* Does a plugin match this particular name? */
-	/* Yes, error out */
+	if (stat_plugin_lookup_by_name(name) != NULL) {
+		fprintf(stderr, "%s: duplicate plugin name (%s)\n",
+		    __func__,
+		    name);
+		return (-1);
+	}
 
 	/* Add it to the plugin list */
+	p = calloc(1, sizeof(*p));
+	if (p == NULL) {
+		warn("%s: calloc", __func__);
+		return (-1);
+	}
+
+	p->state = state;
+	p->name = strdup(name);
+	p->stat_plugin_create = create_func;
+
+	/* Add to the end of the list */
+	TAILQ_INSERT_TAIL(&plugins_list, p, node);
 
 	return (0);
 }
